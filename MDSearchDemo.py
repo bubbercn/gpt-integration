f913528b3004f4b605a840aca7ae8303f4a04f9a
Baosong Wu <bawu@microstrategy.com>
2023-05-25 10:17:09 +0800

    MDSearchDemo.

---
+++ MDSearchDemo.py
@@ -0,0 +1,43 @@
+## setup your OpenAI Key
+import os
+os.environ["OPENAI_API_KEY"] = "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"
+
+from llama_index.indices.struct_store import GPTPandasIndex
+import pandas as pd
+
+df = pd.read_excel("data/DSSMDOBJINFO.xlsx")
+
+index = GPTPandasIndex(df=df)
+
+# do imports
+from langchain.agents import Tool
+from langchain.chains.conversation.memory import ConversationBufferMemory
+from langchain.chat_models import ChatOpenAI
+from langchain.agents import initialize_agent
+
+from llama_index.langchain_helpers.agents import LlamaToolkit, create_llama_chat_agent, IndexToolConfig
+query_engine = index.as_query_engine()
+tool_config = IndexToolConfig(
+    query_engine=query_engine, 
+    name=f"Metadata Info",
+    description=f"useful for when you want to answer queries about the Metadata",
+    tool_kwargs={"return_direct": True}
+)
+toolkit = LlamaToolkit(
+    index_configs=[tool_config]
+)
+memory = ConversationBufferMemory(memory_key="chat_history")
+llm=ChatOpenAI(temperature=0.2,model_name='gpt-3.5-turbo')
+agent_chain = create_llama_chat_agent(
+    toolkit,
+    llm,
+    memory=memory,
+    verbose=True
+)
+while True:
+    text_input = input("User: ")
+    try:
+        response = agent_chain.run(input=text_input)
+    except:
+        print("parse response failure")
+    print(f'Agent: {response}')
